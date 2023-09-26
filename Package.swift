ef39ec61b9d9a84dea343e5b0787c56b503c67c8
Korsun <jenya.korsun@gmail.com>
2023-09-26 15:09:32 +0300

    polishing

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
             targets: ["EasySwiftUI"]),
     ],
     dependencies: [
+        
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
