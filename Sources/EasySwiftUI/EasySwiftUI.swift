86930680a63b62ed8ac7f438796a4a4520cdfb62
Korsun <jenya.korsun@gmail.com>
2023-09-26 14:06:56 +0300

    Initial Commit

---
+++ Sources/EasySwiftUI/EasySwiftUI.swift
@@ -0,0 +1,42 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yevhenii Korsun on 26.09.2023.
+//
+
+import SwiftUI
+
+#if !os(macOS)
+
+public enum EasySwiftUI {
+    static var printPrefix: String = "!@"
+    
+    public static var appBackground: Color = .red
+    public static var appForeground: Color = .red
+    static var navBarColor: Color = .red
+    
+    static var navigationBarHeight: CGFloat = 50
+    static var navigationBarEdges: SwiftUI.EdgeInsets = .init(top: 0, leading: 20, bottom: 0, trailing: 20)
+    static var tabBarHeight: CGFloat = 76
+    
+    public static func configureDefault(
+        printPrefix: String,
+        appBackground: Color,
+        appForeground: Color,
+        navBarColor: Color,
+        navigationBarHeight: CGFloat,
+        navigationBarEdges: SwiftUI.EdgeInsets,
+        tabBarHeight: CGFloat
+    ) {
+        self.printPrefix = printPrefix
+        self.appBackground = appBackground
+        self.appForeground = appForeground
+        self.navBarColor = navBarColor
+        self.navigationBarHeight = navigationBarHeight
+        self.navigationBarEdges = navigationBarEdges
+        self.tabBarHeight = tabBarHeight
+    }
+}
+
+#endif
---
+++ Tests/EasySwiftUITests/EasySwiftUITests.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import EasySwiftUI
+
+final class EasySwiftUITests: XCTestCase {
+
+}
