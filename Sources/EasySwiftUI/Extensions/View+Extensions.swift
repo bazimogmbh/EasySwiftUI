dbb9dd17b5c99e987d0c0f30e2c5a5a22cce69b1
Korsun <jenya.korsun@gmail.com>
2023-09-27 09:35:12 +0300

    add func hideKeyboard

--- Sources/EasySwiftUI/Extensions/View+Extensions.swift
+++ Sources/EasySwiftUI/Extensions/View+Extensions.swift
@@ -131,6 +131,10 @@ public extension View {
             .rotationEffect(Angle(degrees: 180))
             .scaleEffect(x: -1, y: 1, anchor: .center)
     }
+    
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 }
 
 fileprivate struct ScrollButtonStyle: ButtonStyle {
