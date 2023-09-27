afde19c5e62e06c09afef6eaa6e4fc9840f4749d
Korsun <jenya.korsun@gmail.com>
2023-09-27 09:12:53 +0300

    fix FontModifier

--- Sources/EasySwiftUI/Modifiers/FontModifier.swift
+++ Sources/EasySwiftUI/Modifiers/FontModifier.swift
@@ -1,5 +1,5 @@
 //
-//  PageViewStableRotationModifier.swift
+//  FontModifier.swift
 //  EasySwiftUI
 //
 //  Created by Yevhenii Korsun on 25.09.2023.
@@ -9,46 +9,74 @@
 
 import SwiftUI
 
-fileprivate struct PageViewStableRotationModifier<Selection: Hashable>: ViewModifier {
-    @State private var id = UUID()
-    @Binding var selection: Selection
-    
+enum FontType {
+    case ultraLight100
+    case thin200
+    case light300
+    case regular400
+    case medium500
+    case semibold600
+    case bold700
+    case heavy800
+    case black900
+
+    var weight: Font.Weight {
+        switch self {
+        case .ultraLight100: return .ultraLight
+        case .thin200: return .thin
+        case .light300: return .light
+        case .regular400: return .regular
+        case .medium500: return .medium
+        case .semibold600: return .semibold
+        case .bold700: return .bold
+        case .heavy800: return .heavy
+        case .black900: return .black
+        }
+    }
+}
+
+struct FontModifier: ViewModifier {
+    let font: Font
+    let color: Color?
+    let scaleFactor: CGFloat
+
     func body(content: Content) -> some View {
-        content
-            .id(id)
-            .onRotate { _ in
-                let current = selection
-                
-                DispatchQueue.main.async {
-                    id = UUID()
-                    selection = current
-                }
-            }
+        if let color = color {
+            content
+                .font(font)
+                .foregroundColor(color)
+                .minimumScaleFactor(scaleFactor)
+        } else {
+            content
+                .font(font)
+                .minimumScaleFactor(scaleFactor)
+        }
     }
 }
 
-public extension View {
-    func pageViewStableRotation<Selection: Hashable>(selection: Binding<Selection>) -> some View {
-        modifier(PageViewStableRotationModifier(selection: selection))
+extension View {
+    func customFont(_ font: Font, color: Color? = .appForeground, scaleFactor: CGFloat = 0.7) -> some View {
+        self
+            .modifier(FontModifier(font: font, color: color, scaleFactor: scaleFactor))
     }
-}
-
-fileprivate struct DeviceRotationViewModifier: ViewModifier {
-    let action: (UIDeviceOrientation) -> Void
     
-    func body(content: Content) -> some View {
-        content
-            .onAppear()
-            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-                action(UIDevice.current.orientation)
-            }
+    func customFont(_ font: FontType, size: Double, color: Color? = .appForeground, scaleFactor: CGFloat = 0.7) -> some View {
+        self
+            .customFont(.system(size: size, weight: font.weight), color: color, scaleFactor: scaleFactor)
     }
 }
 
-public extension View {
-    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
-        modifier(DeviceRotationViewModifier(action: action))
-    }
-}
+// MARK: - How to use with CustomFont
+
+//enum CustomFont: String {
+//    case someFont = "SomeFontRegular"
+//}
+//
+//extension View {
+//    func customFont(_ font: CustomFont, size: Double, color: Color? = .appForeground, scaleFactor: CGFloat = 0.7) -> some View {
+//        self
+//            .customFont(.custom(font.rawValue, size: size), color: color, scaleFactor: scaleFactor)
+//    }
+//}
 
 #endif
