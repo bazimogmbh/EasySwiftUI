ced399955bebfcf9ac756ceaddcbf8e787a7a4c3
Korsun <jenya.korsun@gmail.com>
2023-09-27 11:51:39 +0300

    add globals

---
+++ Sources/EasySwiftUI/Globals.swift
@@ -0,0 +1,29 @@
+//
+//  Globals.swift
+//  
+//
+//  Created by Yevhenii Korsun on 27.09.2023.
+//
+
+import Foundation
+
+typealias OptionalVoid = (() -> ())?
+typealias OptionalVoidWithError = ((Error) -> ())?
+
+func runOnMainActor(_ body: @MainActor @escaping () -> Void) {
+    Task {
+        await MainActor.run {
+            body()
+        }
+    }
+}
+
+func onMainActorWithAnimation(_ body: @MainActor @escaping () -> Void) {
+    Task {
+        await MainActor.run {
+            withAnimation {
+                body()
+            }
+        }
+    }
+}
