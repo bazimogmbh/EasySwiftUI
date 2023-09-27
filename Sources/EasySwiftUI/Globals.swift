70a7020468f1812ac410f4baded3c8feda4f6fa2
Korsun <jenya.korsun@gmail.com>
2023-09-27 12:05:43 +0300

    add actors tasks

--- Sources/EasySwiftUI/Globals.swift
+++ Sources/EasySwiftUI/Globals.swift
@@ -5,7 +5,30 @@
 //  Created by Yevhenii Korsun on 27.09.2023.
 //
 
-import Foundation
+
+import SwiftUI
 
 typealias OptionalVoid = (() -> ())?
 typealias OptionalVoidWithError = ((Error) -> ())?
+
+#if !os(macOS)
+
+func runOnMainActor(_ body: @MainActor @escaping () -> Void) {
+    Task {
+        await MainActor.run {
+            body()
+        }
+    }
+}
+
+func onMainActorWithAnimation(_ body: @MainActor @escaping () -> Void) {
+    Task {
+        await MainActor.run {
+            withAnimation {
+                body()
+            }
+        }
+    }
+}
+
+#endif
