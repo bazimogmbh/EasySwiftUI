787877832a7ad2593e8a43d3ea84dd89d698b12e
Korsun <jenya.korsun@gmail.com>
2023-10-09 16:01:59 +0300

    change Popover

--- Sources/EasySwiftUI/Views/Popover.swift
+++ Sources/EasySwiftUI/Views/Popover.swift
@@ -9,85 +9,84 @@ import SwiftUI
 
 public struct Popover<Content: View>: View, KeyboardHelper {
     @Environment(\.easyDismiss) private var easyDismiss
+    @State private var isPresented: Bool = false
+    @State private var offsetY: CGFloat = 0
     
-    @State private var showContent = false
-    @State private var showBackground = false
-    @State private var offset: CGFloat = 0.0
+    var dragHeight: CGFloat = 30
     
-    var dragHeight: CGFloat
-    var background: Color
-    var closeAction: () -> ()
+    var closeAction: () -> Void
     @ViewBuilder let content: () -> Content
-    
+
     public init(
         dragHeight: CGFloat = 30,
-        background: Color = Color.black.opacity(0.4),
         closeAction: @escaping () -> Void = {},
         @ViewBuilder content: @escaping () -> Content
     ) {
         self.dragHeight = dragHeight
-        self.background = background
         self.closeAction = closeAction
         self.content = content
     }
     
     public var body: some View {
-        ZStackWithBackground(color: .clear, alignment: .bottom) {
-            if showBackground {
-                background
-                    .ignoresSafeArea()
-                    .transition(.opacity)
-                    .onTapGesture(perform: close)
-            }
-            
-            if showContent {
-                content()
-                    .transition(.move(edge: .bottom))
-                    .zIndex(2)
-                    .environment(\.easyDismiss, EasyDismiss { _ in
-                        close()
-                    })
-                    .onDisappear {
-                        if !showContent {
-                            withAnimation { showBackground = false }
-                            easyDismiss()
+        GeometryReader { geo in
+            Color.black
+                .opacity(isPresented ? 0.7 : 0)
+                .ignoresSafeArea()
+                .animation(.easeInOut, value: isPresented)
+                .onTapGesture {
+                    isPresented = false
+                }
+                .overlay(alignment: .bottom) {
+                    content()
+                        .scaleEffect(scale, anchor: .bottom)
+                        .offset(y: max(0, offsetY))
+                        .overlay(alignment: .top) {
+                            Color.transparent
+                                .frame(height: dragHeight)
+                                .gesture(drag)
                         }
-                    }
-                    .offset(y: offset)
-                    .overlay(alignment: .top, content: dragArea)
+                        .alignmentGuide(.bottom) {
+                            isPresented ? $0[.bottom] : $0[.top] - geo.safeAreaInsets.bottom
+                        }
+                        .animation(.smooth(duration: 0.35), value: isPresented)
+                        .environment(\.easyDismiss, EasyDismiss {_ in
+                            isPresented = false
+                        })
+                }
+        }
+        .transition(.identity)
+        .onChange(of: isPresented) { newValue in
+            if !newValue {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.45) {
+                    closeAction()
+                    easyDismiss()
+                }
             }
         }
         .onAppear {
-            withAnimation {
-                showBackground = true
-                showContent = true
-            }
+            isPresented = true
         }
     }
     
-    private func dragArea() -> some View {
-        Color.transparent
-            .frame(height: dragHeight)
-            .gesture(DragGesture()
-                .onChanged { value in
-                    if value.translation.height > 0 {
-                        self.offset = value.translation.height
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged { val in
+                let height = val.translation.height < 0 ? val.translation.height * 0.2 : val.translation.height
+                offsetY = max(-50, height)
+            }
+            .onEnded { val in
+                if val.translation.height > 100 {
+                    offsetY = 0
+                    isPresented = false
+                } else {
+                    withAnimation {
+                        offsetY = 0
                     }
                 }
-                .onEnded { value in
-                    if value.translation.height > 100 {
-                        close()
-                    } else {
-                        self.offset = 0
-                    }
-                }
-            )
+            }
     }
-
-    private func close() {
-        withAnimation {
-            showContent = false
-            closeAction()
-        }
+    
+    var scale: CGSize {
+        CGSize(width: 1, height: offsetY < 0 ? 1 + abs(offsetY / 1000) : 1)
     }
 }
