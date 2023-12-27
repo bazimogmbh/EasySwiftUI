49991dc025b20f39f0b2ab75ea454066659de464
Korsun <jenya.korsun@gmail.com>
2023-12-27 08:54:07 +0100

    add task extension

---
+++ Sources/EasySwiftUI/Extensions/Task+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yevhenii Korsun on 27.12.2023.
+//
+
+import Foundation
+
+extension Task where Success == Never, Failure == Never {
+    static func sleep(seconds: Double = 1) async throws {
+        let duration = UInt64(seconds * 1_000_000_000)
+        try await Task.sleep(nanoseconds: duration)
+    }
+}
