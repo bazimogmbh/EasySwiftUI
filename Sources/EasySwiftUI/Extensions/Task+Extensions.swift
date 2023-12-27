4085b10e53503c0ed8c6936120d33231e1c5f4a3
Korsun <jenya.korsun@gmail.com>
2023-12-27 08:56:12 +0100

    fix bug

--- Sources/EasySwiftUI/Extensions/Task+Extensions.swift
+++ Sources/EasySwiftUI/Extensions/Task+Extensions.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension Task where Success == Never, Failure == Never {
+public extension Task where Success == Never, Failure == Never {
     static func sleep(seconds: Double = 1) async throws {
         let duration = UInt64(seconds * 1_000_000_000)
         try await Task.sleep(nanoseconds: duration)
