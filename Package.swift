fd6589c8a453b57477dd7131f3a26d2564dc6ffc
Korsun <jenya.korsun@gmail.com>
2023-12-28 09:16:29 +0100

    add include Resources

--- Package.swift
+++ Package.swift
@@ -26,9 +26,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "EasySwiftUI",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Resources")]
+        ),
         .testTarget(
             name: "EasySwiftUITests",
-            dependencies: ["EasySwiftUI"]),
+            dependencies: ["EasySwiftUI"]
+        ),
     ]
 )
