29b99e9e6b8c31209f648d4d9ec52611bca2396c
Korsun <jenya.korsun@gmail.com>
2024-01-29 17:22:53 +0100

    add EasyDismissContainer add SheetContainer

---
+++ Sources/EasySwiftUI/Views/EasyDismissContainer.swift
@@ -0,0 +1,30 @@
+//
+//  EasyDismissContainer.swift
+//
+//
+//  Created by Yevhenii Korsun on 29.01.2024.
+//
+
+import SwiftUI
+
+public struct EasyDismissContainer<Content: View>: View {
+    @Environment(\.easyDismiss) var easyDismiss
+    @State private var isShow: Bool = false
+    let content: (Binding<Bool>) -> Content
+    
+    public init(content: @escaping (Binding<Bool>) -> Content) {
+        self.content = content
+    }
+    
+    public var body: some View {
+        content($isShow)
+            .onAppear {
+                isShow = true
+            }
+            .onChange(of: isShow) {
+                if !$0 {
+                    easyDismiss()
+                }
+            }
+    }
+}
---
+++ Sources/EasySwiftUI/Views/SheetContainer.swift
@@ -0,0 +1,26 @@
+//
+//  SheetContainer.swift
+//  
+//
+//  Created by Yevhenii Korsun on 29.01.2024.
+//
+
+import SwiftUI
+
+public struct SheetContainer<Content: View>: View {
+    let content: () -> Content
+    
+    init(content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    public var body: some View {
+        EasyDismissContainer {
+            Color.clear
+                .sheet(isPresented: $0) {
+                    content()
+                        .ignoresSafeArea()
+                }
+        }
+    }
+}
