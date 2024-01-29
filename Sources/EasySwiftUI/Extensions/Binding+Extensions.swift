1c7d0c49c849546f6b20e904c8b3fa0a73958ad0
Korsun <jenya.korsun@gmail.com>
2024-01-29 16:12:59 +0100

    add func map to Binding

--- Sources/EasySwiftUI/Extensions/Binding+Extensions.swift
+++ Sources/EasySwiftUI/Extensions/Binding+Extensions.swift
@@ -5,4 +5,15 @@
 //  Created by Yevhenii Korsun on 29.01.2024.
 //
 
-import Foundation
+import SwiftUI
+
+public extension Binding {
+    @MainActor
+    func map(_ completion: @escaping (Value) -> Value) -> Self {
+        Task {
+            wrappedValue = completion(wrappedValue)
+        }
+        
+        return self
+    }
+}
